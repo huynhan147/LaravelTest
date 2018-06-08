2c505a546f15ab44eff04c7009b177e6fcda2c18
huynhan147 <huyitptit2015@gmail.com>
2018-06-08 11:18:34 +0700

    admins

--- resources/views/admins/sidebar.blade.php
+++ resources/views/admins/sidebar.blade.php
@@ -4,6 +4,12 @@
             <div class="sidebar-sticky">
                 <ul class="nav flex-column">
                     <li class="nav-item">
+                        <a class="nav-link active" href="/admins">
+                            <span data-feather="home"></span>
+                            List Post <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link active" href="/admins/province">
                             <span data-feather="home"></span>
                             Dashboard <span class="sr-only">(current)</span>
