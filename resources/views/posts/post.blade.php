16fe09883910dff222eeb0007adf380e54df22d6
huynhan147 <huyitptit2015@gmail.com>
2018-06-09 04:52:59 +0700

    lgoin

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -3,11 +3,11 @@
         <a href="/posts/{{ $post->id }}">
             {{$post->title}}
         </a>
-        {{--@if(Auth::check() && auth()->user()->id == $post->user->id)--}}
 
-        {{--@endif--}}
     </h2>
+    @if(Auth::check() && auth()->user()->id == $post->user->id)
     <span class="blog-post-title"> &nbsp; <a href="/admins/post-delete/{{ $post->id }}"><i class="fas fa-trash-alt"></i></a></span>
+    @endif
     <p class="blog-post-meta">
         {{ $post->user->name }} on
         {{ $post->created_at->toDayDateTimeString() }}</p>
