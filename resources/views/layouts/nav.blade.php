3d946575adb56c7f93179d841477183326a00423
huynhan147 <huyitptit2015@gmail.com>
2018-06-08 17:19:26 +0700

    up

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -13,8 +13,8 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
             </a>
             @if(Auth::check())
-                <a class="btn btn-sm btn-outline-secondary" href="/logout">Log out</a>
-                <a class="btn btn-sm btn-outline-secondary" href="/admins/post/create">Create Post</a>
+                <a class="btn btn-sm btn-outline-secondary mx-1" href="/logout">Log out</a>
+                <a class="btn btn-sm btn-outline-secondary mx-1" href="/admins/post/create">Create Post</a>
             @else
                 <a class="btn btn-sm btn-outline-secondary" href="/login">Sign in</a>
                 <a class="btn btn-sm btn-outline-secondary" href="/register">Register</a>
