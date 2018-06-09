3e5723126a8862c08bad802a35ea1dfc3d8d7809
huynhan147 <huyitptit2015@gmail.com>
2018-06-09 09:27:35 +0700

    promiss auth

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -26,7 +26,7 @@
                 <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
 
             </div>
-            <input type="text" class="role" id="role">
+            <input type="text" class="role" id="role" name="role">
             <button type="submit" class="btn btn-primary" name="btnsubmit">Đăng ký</button>
         </form>
         @include('layouts.errors');
